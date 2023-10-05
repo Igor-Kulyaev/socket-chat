4f314b7f889481807a4241686f52ed893469da34
Igor Kulyaev <kulyaev.igor@gmail.com>
2023-10-05 15:32:22 +0300

    Create frontend with nextjs

---
+++ server/src/models/message.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  username:{
+    type:String,
+    unique: true,
+    required: true,
+  },
+  firstName:{
+    type:String,
+    required: true,
+  },
+  lastName:{
+    type:String,
+    required: true,
+  },
+  email:{
+    type:String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type:String,
+    required: true,
+  },
+  role: {
+    type:String,
+    enum: ["user", "admin"],
+    required: true,
+  },
+  authType:{
+    type:String,
+    enum: ["internal", "google", "facebook"],
+    required: true,
+  }
+});
+
+module.exports = mongoose.model("User", userSchema);
