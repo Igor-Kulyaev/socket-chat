4121510b6488e02b2611ec3379cbc4c53afb7edd
Igor Kulyaev <kulyaev.igor@gmail.com>
2023-10-05 17:45:51 +0300

    Create passportjs middleware for login

---
+++ server/src/utils/auth/authUtils.js
@@ -0,0 +1,30 @@
+// passport.js
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const config = require('../../config/config'); // Your JWT configuration file
+
+const User = require('../../models/user'); // Assuming your user model is in a separate file
+
+const options = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: config.jwtSecret,
+};
+
+passport.use(
+  new JwtStrategy(options, async (jwtPayload, done) => {
+    try {
+      const user = await User.findOne({ username: jwtPayload.username });
+
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    } catch (error) {
+      return done(error, false);
+    }
+  })
+);
+
+module.exports = passport;
