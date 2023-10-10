5b345fbe39dd8d8c05ab12c2a16bc41155b05793
Igor Kulyaev <kulyaev.igor@gmail.com>
2023-10-10 12:32:39 +0300

    Create cors config

---
+++ server/src/config/corsConfig.js
@@ -0,0 +1,6 @@
+module.exports = {
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowHeaders: 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers',
+  credentials: true
+};
