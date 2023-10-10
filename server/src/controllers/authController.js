f02d6ac361ffbad97b1c785ad4b6f5abe179b8de
Igor Kulyaev <kulyaev.igor@gmail.com>
2023-10-10 12:32:29 +0300

    Create controllers

---
+++ server/src/controllers/authController.js
@@ -0,0 +1,156 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const {createJWTPayload, validateRefreshToken} = require("../utils/utils");
+const jwtConfig = require("../config/jwtConfig");
+const User = require("../models/user");
+const {Token} = require("../models/token");
+const ApplicationError = require("../utils/error/ApplicationError");
+const registerUser = async (req, res, next) => {
+  try {
+    const existingUser = await User.findOne({ username: req.body.username });
+    if (existingUser) {
+      throw new ApplicationError(400, 'Username is taken');
+    }
+
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    const newUser = new User({
+      username: req.body.username,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: hashedPassword,
+      role: 'user',
+      authType: 'internal',
+    });
+
+    const savedUser = await newUser.save();
+    const payload = createJWTPayload(savedUser);
+
+    const accessToken = jwt.sign(payload, jwtConfig.jwtAccessSecret, {
+      expiresIn: '10m',
+    });
+    const refreshToken = jwt.sign(payload, jwtConfig.jwtRefreshSecret, {
+      expiresIn: '15m',
+    });
+
+    const token = new Token({
+      userId: savedUser._id,
+      refreshToken: refreshToken,
+    });
+
+    // Save the token to the database
+    const savedToken = await token.save();
+
+    res.cookie('refreshToken', refreshToken, {maxAge: 720000, httpOnly: true});
+    res.status(201).json({ token: accessToken });
+  } catch (error) {
+    console.error('Error during registration:', error);
+    next(error);
+  }
+}
+
+const loginUser = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      throw new ApplicationError(404, 'User not found');
+    }
+
+    const passwordMatch = await bcrypt.compare(req.body.password, user.password);
+    if (!passwordMatch) {
+      throw new ApplicationError(401, 'Incorrect password');
+    }
+
+    const payload = createJWTPayload(user);
+
+    const accessToken = jwt.sign(payload, jwtConfig.jwtAccessSecret, {
+      expiresIn: '10m',
+    });
+    const refreshToken = jwt.sign(payload, jwtConfig.jwtRefreshSecret, {
+      expiresIn: '15m',
+    });
+
+    const updatedOrCreatedToken = await Token.findOneAndUpdate(
+      { userId: user._id },
+      {
+        refreshToken: refreshToken,
+        updatedAt: new Date(),
+      },
+      {
+        upsert: true,
+        new: true,
+      }
+    )
+
+    console.log('refreshToken', refreshToken);
+
+    res.cookie('refreshToken', refreshToken, {maxAge: 720000, httpOnly: true});
+    res.status(200).json({ token: accessToken });
+  } catch (error) {
+    console.error('Error during login:', error);
+    next(error);
+  }
+}
+
+const logoutUser = async (req, res, next) => {
+  try {
+    const {refreshToken} = req.cookies;
+    const {_id} = validateRefreshToken(refreshToken);
+    await Token.deleteOne({ userId: _id });
+    res.clearCookie('refreshToken');
+    res.status(200).json({message: 'User has logged out'});
+  } catch (error) {
+    console.error('Error during logout:', error);
+    next(error);
+  }
+}
+
+const refreshToken = async (req, res, next) => {
+  try {
+    const {refreshToken} = req.cookies;
+    if (!refreshToken) {
+      throw new ApplicationError(401, 'User is not authorized');
+    }
+    const tokenPayload = validateRefreshToken(refreshToken);
+    const tokenFromDb = await Token.findOne({ userId: tokenPayload._id });
+
+    console.log('tokenFromDb', tokenFromDb);
+
+    if (!tokenFromDb) {
+      throw new ApplicationError(401, 'User is not authorized');
+    }
+
+    const newAccessToken = jwt.sign(createJWTPayload(tokenPayload), jwtConfig.jwtAccessSecret, {
+      expiresIn: '10m',
+    });
+    const newRefreshToken = jwt.sign(createJWTPayload(tokenPayload), jwtConfig.jwtRefreshSecret, {
+      expiresIn: '15m',
+    });
+
+    const updatedToken = await Token.findOneAndUpdate(
+      { userId: tokenPayload._id },
+      {
+        refreshToken: refreshToken,
+        updatedAt: new Date(),
+      },
+      {
+        upsert: false,
+        new: true,
+      }
+    )
+
+    res.cookie('refreshToken', newRefreshToken, {maxAge: 720000, httpOnly: true});
+    res.status(200).json({ token: newAccessToken });
+  } catch (error) {
+    console.error('Error during refresh:', error);
+    next(error);
+  }
+}
+
+module.exports = {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshToken,
+}
--- server/src/router/authRoutes.js
+++ server/src/router/authRoutes.js
@@ -1,155 +1,19 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
-const { Token } = require('../models/token');
-const config = require('../config/config');
-const {createJWTPayload, validateRefreshToken} = require("../utils/utils");
-const ApplicationError = require("../utils/error/ApplicationError"); // Your JWT configuration file
+const passport = require("../utils/passportjs/passport");
+const {registerUser, loginUser, logoutUser, refreshToken} = require("../controllers/authController"); // Your JWT configuration file
 
 const router = express.Router();
 
-router.post('/register', async (req, res, next) => {
-  try {
-    const existingUser = await User.findOne({ username: req.body.username });
-    if (existingUser) {
-      throw new ApplicationError(400, 'Username is taken');
-    }
+router.post('/register', registerUser);
 
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+router.post('/login', loginUser);
 
-    const newUser = new User({
-      username: req.body.username,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-      password: hashedPassword,
-      role: 'user',
-      authType: 'internal',
-    });
+router.post('/logout', logoutUser);
 
-    const savedUser = await newUser.save();
-    const payload = createJWTPayload(savedUser);
+router.get('/refresh', refreshToken);
 
-    const accessToken = jwt.sign(payload, config.jwtAccessSecret, {
-      expiresIn: '10m',
-    });
-    const refreshToken = jwt.sign(payload, config.jwtRefreshSecret, {
-      expiresIn: '15m',
-    });
-
-    const token = new Token({
-      userId: savedUser._id,
-      refreshToken: refreshToken,
-    });
-
-    // Save the token to the database
-    const savedToken = await token.save();
-
-    res.cookie('refreshToken', refreshToken, {maxAge: 720000, httpOnly: true});
-    res.status(201).json({ token: accessToken });
-  } catch (error) {
-    console.error('Error during registration:', error);
-    next(error);
-  }
-});
-
-router.post('/login', async (req, res, next) => {
-  try {
-    const user = await User.findOne({ username: req.body.username });
-    if (!user) {
-      throw new ApplicationError(404, 'User not found');
-    }
-
-    const passwordMatch = await bcrypt.compare(req.body.password, user.password);
-    if (!passwordMatch) {
-      throw new ApplicationError(401, 'Incorrect password');
-    }
-
-    const payload = createJWTPayload(user);
-
-    const accessToken = jwt.sign(payload, config.jwtAccessSecret, {
-      expiresIn: '1m',
-    });
-    const refreshToken = jwt.sign(payload, config.jwtRefreshSecret, {
-      expiresIn: '1m',
-    });
-
-    const updatedOrCreatedToken = await Token.findOneAndUpdate(
-      { userId: user._id },
-      {
-        refreshToken: refreshToken,
-        updatedAt: new Date(),
-      },
-      {
-        upsert: true,
-        new: true,
-      }
-    )
-
-    console.log('refreshToken', refreshToken);
-
-    res.cookie('refreshToken', refreshToken, {maxAge: 720000, httpOnly: true});
-    res.status(200).json({ token: accessToken });
-  } catch (error) {
-    console.error('Error during login:', error);
-    next(error);
-  }
-});
-
-router.post('/logout', async (req, res, next) => {
-  try {
-    const {refreshToken} = req.cookies;
-    const {_id} = validateRefreshToken(refreshToken);
-    await Token.deleteOne({ userId: _id });
-    res.clearCookie('refreshToken');
-    res.status(200).json({message: 'User has logged out'});
-  } catch (error) {
-    console.error('Error during logout:', error);
-    next(error);
-  }
-});
-
-router.get('/refresh', async (req, res, next) => {
-  try {
-    const {refreshToken} = req.cookies;
-    if (!refreshToken) {
-      throw new ApplicationError(401, 'User is not authorized');
-    }
-    const tokenPayload = validateRefreshToken(refreshToken);
-    const tokenFromDb = await Token.findOne({ userId: tokenPayload._id });
-
-    console.log('tokenFromDb', tokenFromDb);
-
-    if (!tokenFromDb) {
-      throw new ApplicationError(401, 'User is not authorized');
-    }
-
-    const newAccessToken = jwt.sign(createJWTPayload(tokenPayload), config.jwtAccessSecret, {
-      expiresIn: '10m',
-    });
-    const newRefreshToken = jwt.sign(createJWTPayload(tokenPayload), config.jwtRefreshSecret, {
-      expiresIn: '15m',
-    });
-
-    const updatedToken = await Token.findOneAndUpdate(
-      { userId: tokenPayload._id },
-      {
-        refreshToken: refreshToken,
-        updatedAt: new Date(),
-      },
-      {
-        upsert: false,
-        new: true,
-      }
-    )
-
-    res.cookie('refreshToken', newRefreshToken, {maxAge: 720000, httpOnly: true});
-    res.status(200).json({ token: newAccessToken });
-  } catch (error) {
-    console.error('Error during refresh:', error);
-    next(error);
-  }
+router.get('/verify-token', passport.authenticate('jwt', {session: false}), (req, res) => {
+  res.status(200).json({message: 'Verified'});
 });
 
 module.exports = router;
