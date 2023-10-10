864322be2b9c3b434c44887f389eacbc4395e751
Igor Kulyaev <kulyaev.igor@gmail.com>
2023-10-10 11:16:07 +0300

    Add redirect for main page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,21 +2,23 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import {useRouter} from "next/navigation";
+import {useEffect} from "react";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Socket Chat App</title>
-        <meta name="description" content="Socket Chat Application" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={`${styles.main} ${inter.className}`}>
+  const router = useRouter();
+  // const token = localStorage.getItem("token");
 
-      </main>
-    </>
-  )
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (!token) {
+      router.push("/auth");
+    } else {
+      router.push("/chat");
+    }
+  }, []);
+
+  return null;
 }
