c0caf11f1e203acf1989ba2febea83a8bd1bbbd5
Igor Kulyaev <kulyaev.igor@gmail.com>
2023-10-11 11:49:03 +0300

    Add authorization provider

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -7,12 +7,9 @@ import {Button, TextField} from "@mui/material";
 import { useForm } from "react-hook-form";
 import api from "@/utils/api";
 import {useRouter} from "next/router";
-
-function AuthSkeleton() {
-  return (
-    <div>Auth skeleton</div>
-  )
-}
+import {decryptToken, encryptToken, USER_IP} from "@/utils/encryption";
+import {AuthSkeleton} from "@/components/AuthSkeleton";
+import {useAuthorization} from "@/hooks/useAuthorization";
 
 function AuthTabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -37,15 +34,16 @@ function AuthTabPanel(props) {
 function AuthRegistration() {
   const { register, handleSubmit, watch, formState: { errors } } = useForm();
   const router = useRouter();
+  const {authUser, setAuthUser} = useAuthorization();
   const onSubmit = async (data) => {
-    console.log(data);
     try {
       const result = await api.post('register', data, );
-      console.log('result', result);
+      // const encryptedToken = encryptToken(result.data.token, USER_IP.IP_ADDRESS);
       localStorage.setItem(
         "token",
         result.data.token
       );
+      setAuthUser(result.data.user);
       await router.push("/chat");
 
     } catch (error) {
@@ -81,15 +79,16 @@ function AuthRegistration() {
 function AuthLogin() {
   const { register, handleSubmit, watch, formState: { errors } } = useForm();
   const router = useRouter();
+  const {authUser, setAuthUser} = useAuthorization();
   const onSubmit = async (data) => {
-    console.log(data);
     try {
       const result = await api.post('login', data, );
-      console.log('result', result);
+      // const encryptedToken = encryptToken(result.data.token, USER_IP.IP_ADDRESS);
       localStorage.setItem(
         "token",
         result.data.token
       );
+      setAuthUser(result.data.user);
       await router.push("/chat");
 
     } catch (error) {
@@ -123,7 +122,9 @@ export default function AuthorizationComponent() {
   };
 
   useEffect(() => {
-    const token = localStorage.getItem('token')
+    // const encryptedToken = localStorage.getItem('token');
+    // const decryptedToken = encryptedToken ? decryptToken(encryptedToken, USER_IP.IP_ADDRESS) : null;
+    const token = localStorage.getItem('token');
     if (token) {
       router.push("/chat");
     } else {
