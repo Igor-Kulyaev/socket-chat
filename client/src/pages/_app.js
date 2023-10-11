6316baae9262f2249c61ad481ffbf65d95f2d495
Igor Kulyaev <kulyaev.igor@gmail.com>
2023-10-11 11:48:46 +0300

    Add authorization provider

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,11 +1,19 @@
 import '@/styles/reset.css'
 import '@/styles/globals.css'
 import {USER_IP} from "@/utils/encryption";
+import {AuthorizationProvider} from "@/providers/AuthorizationContext/AuthorizationProvider";
 
-fetch('https://api.ipify.org?format=json')
-  .then(response => response.json())
-  .then(data => USER_IP.IP_ADDRESS = data.ip);
+// Determine user's IP for encrypting/decrypting token later for security reasons
+// fetch('https://api.ipify.org?format=json')
+//   .then(response => response.json())
+//   .then(data => USER_IP.IP_ADDRESS = data.ip);
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <AuthorizationProvider>
+        <Component {...pageProps} />
+      </AuthorizationProvider>
+    </>
+  )
 }
