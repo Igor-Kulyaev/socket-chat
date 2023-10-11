96983b9b78e8cbf1263abbe5a521db13b24a8491
Igor Kulyaev <kulyaev.igor@gmail.com>
2023-10-11 11:50:24 +0300

    Removed odd files

--- client/src/components/GeneralLayout.jsx
+++
@@ -1,9 +0,0 @@
-
-
-export const GeneralLayout = () => {
-  return (
-    <div>
-
-    </div>
-  )
-}
--- client/src/components/GeneralSkeleton.jsx
+++
@@ -1,5 +0,0 @@
-export const GeneralSkeleton = () => {
-  return (
-    <div>General Skeleton</div>
-  )
-}
